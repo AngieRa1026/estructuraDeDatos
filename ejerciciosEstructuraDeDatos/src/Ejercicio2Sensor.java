1f12a7e0f26ca3053d5015c8f710cdce3783ea26
Angie Ramirez <angieramirez1026@gmail.com>
2026-02-26 17:21:50 -0500

    Segundo ejercicio Sensor

--- ejerciciosEstructuraDeDatos/src/Ejercicio2Sensor.java
+++ ejerciciosEstructuraDeDatos/src/Ejercicio2Sensor.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
 
 public class Ejercicio2Sensor {
-    Scanner scanner = new Scanner(System.in);
-    double[][] temperatura;
-    
-    public void cargarTemperatura() {
+    static Scanner scanner = new Scanner(System.in);
+    static double[][] temperatura = new double[3][3];
+
+    public static void main(String[] args) {
+        cargarTemperatura();
+        System.out.println("\n--- Resultados por Zona ---");
+        promedioPorZona();
+        System.out.println("\n--- Resultado General ---");
+        System.out.println("Promedio General de la planta: " + promedioGeneral());
+    }
+
+    public static void cargarTemperatura() {
         for (int i = 0; i < temperatura.length; i++) {
             for (int j = 0; j < temperatura[i].length; j++) {
                 System.out.print("Zona " + (i + 1) + " - Sensor " + (j + 1) + ": ");
@@ -12,7 +20,8 @@ public class Ejercicio2Sensor {
             }
         }
     }
-    public void promedioPorZona() {
+
+    public static void promedioPorZona() {
         for (int i = 0; i < temperatura.length; i++) {
             double suma = 0;
             for (int j = 0; j < temperatura[i].length; j++) {
@@ -22,16 +31,14 @@ public class Ejercicio2Sensor {
             System.out.println("Promedio de la Zona " + (i + 1) + ": " + promedio);
         }
     }
-    public double promedioGeneral(){
+
+    public static double promedioGeneral() {
         double suma = 0;
         for (int i = 0; i < temperatura.length; i++) {
             for (int j = 0; j < temperatura[i].length; j++) {
                 suma += temperatura[i][j];
-    
-    }
+            }
         }
         return suma / (temperatura.length * temperatura[0].length);
     }
 }
-    
-
